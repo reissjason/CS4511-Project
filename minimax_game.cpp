5f5fb1bfa1805b3d59d5977faafc21faf6b632d1
Jason Reiss <reiss.jason@gmail.com>
2012-05-05 23:16:24 -0500

    added code to stop minimax from switching every turn or picking a less powerful attack

--- minimax_game.cpp
+++ minimax_game.cpp
@@ -125,10 +125,6 @@ TinyAttack * string_to_attack(string move_name){
 
 int have_a_fight() {
 
-	cout << "Choose your Pokemon!" <<endl;
-
-	cout << "Choose your Enemy!" << endl;
-
 	string pokemons[] = { "charizard",
 			"roserade",
 			"slowbro",
@@ -143,7 +139,7 @@ int have_a_fight() {
 		int num = rand() % 6;
 		stringstream ss;
 		ss << num;
-		if (choices.find_first_of(ss.str()) == string::npos)
+		//if (choices.find_first_of(ss.str()) == string::npos)
 			choices += ss.str();
 	}
 
@@ -156,13 +152,13 @@ int have_a_fight() {
 			choose(pokemons[(choices[5])-48]), NULL, NULL, NULL);
 
 	cout << "Teams have been chosen!" << endl;
-
+	cout << "Choose your Pokemon!" <<endl;
 	cout << "Team 1 : " << laptop->isAlive() << endl;
 	cout << pokemons[(choices[0])-48] << endl;
 	cout << pokemons[(choices[1])-48] << endl;
 	cout << pokemons[(choices[2])-48] << endl;
 	cout << endl;
-
+	cout << "Choose your Enemy!" << endl;
 	cout << "Team 2 : " << tower->isAlive() << endl;
 	cout << pokemons[(choices[3])-48] << endl;
 	cout << pokemons[(choices[4])-48] << endl;
@@ -181,7 +177,11 @@ int have_a_fight() {
 	int round_num = 1;
 	int damage = 0;
 	int attack_num = 1;
+	bool dont_switch = false;
+	bool switched_last_round = false;
 
+
+	TinyAttack *last_move = NULL;
 
 	while (laptop->isAlive() && tower->isAlive()) {
 		State *s = new State(sim, 0);
@@ -201,19 +201,31 @@ int have_a_fight() {
 		int minimaxValue = turn_minimax(&tree);
 		cout << "Minimax Returns: " << minimaxValue << endl;
 		State* minimaxRecommends = buildAnswer(&tree, minimaxValue);
+
+		cout << "MINIMAX RECOMMENDS: " << minimaxRecommends->getActionUsed() << endl;
+		attack_num = minimaxRecommends->getActionUsed();
+		next_move = alist->string_to_attack(attacker->get_attack(attack_num));
 		//cout << "test = " << minimaxRecommends->myPokemon->get_name() << endl;
-		if (minimaxRecommends->myPokemon->get_name().compare(attacker->get_name()) != 0) {
+		if (last_move != NULL)
+			cout << "last move: " << last_move->get_power() << endl;
+		cout << "next move: " << next_move->get_power() << endl;
+
+		if (last_move != NULL && last_move->get_power() >= next_move->get_power()) {
+			next_move = last_move;
+			dont_switch = true;
+		} else {
+			dont_switch = false;
+		}
+
+		if (!switched_last_round && !dont_switch && minimaxRecommends->myPokemon->get_name().compare(attacker->get_name()) != 0) {
 			cout << "switching to " << minimaxRecommends->myPokemon->get_name() << endl;
 			sim->get_player()->change_lead(minimaxRecommends->myPokemon);
 			attacker = sim->get_player()->get_lead();
+			switched_last_round = true;
 		} else {
-			cout << "MINIMAX RECOMMENDS: " << minimaxRecommends->getActionUsed() << endl;
-			attack_num = minimaxRecommends->getActionUsed();
-
 			cout << "That player has an attack!" << endl;
 			cout << "look for attack #" << attack_num << endl;
 			cout << attacker->get_attack(attack_num) << endl;
-			next_move = alist->string_to_attack(attacker->get_attack(attack_num));
 
 			cout << "That attack has power = " << next_move->get_power() << "!" << endl;
 			cout << attacker->get_name() << " attacks " << defender->get_name() << " with " << next_move->get_name() << endl;
@@ -222,8 +234,8 @@ int have_a_fight() {
 			cout << "damage = " << damage << endl;
 
 			defender->reduce_hp(damage);
-
-
+			last_move = next_move;
+			switched_last_round = false;
 		}
 
 		delete minimaxRecommends;
@@ -231,14 +243,24 @@ int have_a_fight() {
 
 
 		attack_num = rand() % 4 + 1;
+		int poke_num = attack_num - 4;
+		cout << "attack_num : " << attack_num << " poke_num : " << poke_num << endl;
+		if (attack_num < 5 || tower->get_bench(poke_num)->get_name().compare(defender->get_name()) == 0) {
+			if (attack_num >= 5) attack_num -= 3;
+			cout << "attack_num : " << attack_num << endl;
+			next_move = alist->string_to_attack(defender->get_attack(attack_num));
 
-		next_move = alist->string_to_attack(defender->get_attack(attack_num));
-		cout << defender->get_name() << " attacks " << attacker->get_name() << " with " << next_move->get_name() << endl;
-		damage = sim->calculate_damage(next_move, defender, attacker);
-		cout << "damage = " << damage << endl;
+			cout << defender->get_name() << " attacks " << attacker->get_name() << " with " << next_move->get_name() << endl;
+			damage = sim->calculate_damage(next_move, defender, attacker);
+			cout << "damage = " << damage << endl;
 
-		attacker->reduce_hp(damage);
+			attacker->reduce_hp(damage);
+		} else {
 
+			cout << "switching to " << tower->get_bench(poke_num)->get_name() << endl;
+			tower->change_lead(tower->get_bench(poke_num));
+
+		}
 		round_num++;
 		cout << "Round " << round_num << endl;
 		cout << " " << attacker->get_name() << " hp: " << attacker->get_current_hp() << endl;
